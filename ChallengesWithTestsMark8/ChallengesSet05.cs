2078aff940bc8fdc9ae6fc9ca4c98a29962361f6
Enmanuel Ozoria <ozoriaeman1998@gmail.com>
2022-11-03 11:27:43 -0400

    Completed GetNextNumberDivisibleByN, ChangeNamesOfBusinessesWithNoRevenueTo_CLOSED, IsAscendingOrder, SumElementsThatFollowAnEven, and GetEveryFourthElement methods in ChallengeSet05

--- ChallengesWithTestsMark8/ChallengesSet05.cs
+++ ChallengesWithTestsMark8/ChallengesSet05.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace ChallengesWithTestsMark8
 {
@@ -7,32 +8,74 @@ namespace ChallengesWithTestsMark8
     {
         public int GetNextNumberDivisibleByN(int startNumber, int n)
         {
-            throw new NotImplementedException();
+            int num = startNumber + 1;
+            while (true)
+            {
+                if (num % n == 0)
+                {
+                    return num;
+                }
+                num++;
+            }
         }
 
         public void ChangeNamesOfBusinessesWithNoRevenueTo_CLOSED(Business[] businesses)
         {
-            throw new NotImplementedException();
+            businesses.Where(x => x.TotalRevenue == 0).ToList().ForEach(x => x.Name = "CLOSED");            
         }
 
         public bool IsAscendingOrder(int[] numbers)
         {
-            throw new NotImplementedException();
+            if (numbers == null || numbers.Length == 0)
+            {
+                return false;
+            }
+            var ascenNums = numbers.OrderBy(x => x);
+            return Enumerable.SequenceEqual(numbers, ascenNums);
         }
 
         public int SumElementsThatFollowAnEven(int[] numbers)
-        {
-            throw new NotImplementedException();
+        {            
+            int sum = 0;
+            if (numbers == null)
+            {
+                return 0;
+            }
+            for (int i = 1; i < numbers.Length; i++)
+            {
+                if (numbers[i - 1] % 2 == 0)
+                {
+                    sum += numbers[i];
+                }
+            }
+            return sum;
         }
 
         public string TurnWordsIntoSentence(string[] words)
         {
-            throw new NotImplementedException();
+            //Incomplete
+            if (words == null || words.Length == 0)
+            {
+                return "";
+            }
+            return string.Join(" ", words) + ".";
         }
 
         public double[] GetEveryFourthElement(List<double> elements)
         {
-            throw new NotImplementedException();
+            if (elements == null)
+            {
+                return new double[0];
+            }
+            var elms = new List<double>();
+            for (int i = 0; i < elements.Count; i++)
+            {
+                if ((i+1) % 4 == 0)
+                {
+                    elms.Add(elements[i]);
+                }
+            }
+            return elms.ToArray();
         }
 
         public bool TwoDifferentElementsInArrayCanSumToTargetNumber(int[] nums, int targetNumber)
